8d0afe2c860e1627693ae8129afc2458d728fb4c
Keheliya-Sathsara <keheliyasm@gmail.com>
2026-04-03 14:52:52 +0530

    changs to the README file to include installation instructions and usage examples.

---
+++ .angular/cache/19.2.1/kapital-corp/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,16 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-AOCBQEO5.js";
+import "./chunk-WXJALMVD.js";
+import "./chunk-U62FLTWO.js";
+import "./chunk-P6U2JBMQ.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
